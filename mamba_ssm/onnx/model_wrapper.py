4c7f8e970cc7304eddb6352121252ee63ef9fcb2
Danilo Santitto <danilo.santitto@gmail.com>
2024-02-12 13:12:53 +0100

    feat: add mamba model wrapper

--- mamba_ssm/onnx/model_wrapper.py
+++ mamba_ssm/onnx/model_wrapper.py
@@ -3,7 +3,7 @@ import torch
 from mamba_ssm.models.config_mamba import MambaConfig
 from mamba_ssm.utils.generation import InferenceParams
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel, create_block
-
+from mamba_ssm.modules.mamba_simple import Mamba
 class ModelWrapper(torch.nn.Module):
     def __init__(self, model_name=None, use_generation=False, config: MambaConfig=None, device='cpu', dtype=torch.float32):
         super(ModelWrapper, self).__init__()
@@ -63,4 +63,21 @@ class BlockModelWrapper(torch.nn.Module):
         dummy_inference_params = InferenceParams(max_seqlen=max_length, max_batch_size=batch_size)
 
         output, _ = self.model(hidden_states, residual, dummy_inference_params)
+        return output
+
+class MambaModelWrapper(torch.nn.Module):
+    def __init__(self, config: MambaConfig=None, device='cpu', dtype=torch.float32):
+        super(MambaModelWrapper, self).__init__() 
+        self.d_model = config.d_model
+        self.model = Mamba(config.d_model, layer_idx=0, device=device, dtype=dtype)
+        print(f"Size of d: {self.d_model}")
+        print(f"Number of parameters: {sum(p.numel() for p in self.model.parameters() if p.requires_grad)}")
+    
+    def forward(self, input_ids, hidden_states):
+        
+        batch_size, _ = input_ids.shape
+        max_length = input_ids.shape[1] + 100
+        dummy_inference_params = InferenceParams(max_seqlen=max_length, max_batch_size=batch_size)
+
+        output = self.model(hidden_states, dummy_inference_params)
         return output
